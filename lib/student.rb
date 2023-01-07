35077fe4e382e1ab70916226f515104a3dff235b
Daria Approvato <dariaapprovato@gmail.com>
2023-01-07 14:09:58 -0500

    commit tree ea25ccb9310de2b272dde3d9fda986eeca53cbe7

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = Array.new
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 
 end
